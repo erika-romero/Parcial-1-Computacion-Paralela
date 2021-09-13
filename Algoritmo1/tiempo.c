a87a3fdf11e9dfbf4afe632935f3996fe2c4790b
Erika Romero <84427277+erika-romero@users.noreply.github.com>
2021-09-12 19:44:35 -0500

    Add files via upload

---
+++ Algoritmo1/tiempo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include "tiempo.h"
+
+struct timeval inicio; 
+struct timeval final;
+
+void funcionInicio(){
+    gettimeofday(&inicio, NULL); 
+}
+
+void funcionFinal(){
+    gettimeofday(&final, NULL); 
+}
+
+void impresionTiempo(){
+
+    final.tv_sec -= inicio.tv_sec;
+    printf("%f", (double)(final.tv_sec*1000000 + final.tv_usec));
+    printf("\n"); 
+}
